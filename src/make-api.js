f06c0de515219aed8d8c53f2f30fd31161320d0a
Christopher De Cairos <cade@mozillafoundation.org>
2014-01-09 12:54:00 -0800

    Add remixurl and editurl to wrapped makes

--- src/make-api.js
+++ src/make-api.js
@@ -221,7 +221,7 @@ var module = module || undefined;
     [ "url", "contentType", "locale", "title",
       "description", "author", "published", "tags", "thumbnail",
       "username", "remixedFrom", "_id", "emailHash", "createdAt",
-      "updatedAt", "likes", "reports" ].forEach( function( prop ) {
+      "updatedAt", "likes", "reports", "remixurl", "editurl" ].forEach( function( prop ) {
         wrapped[ prop ] = make[ prop ];
     });
 
